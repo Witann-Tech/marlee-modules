5f56c176a5ce85927b1fa8cb121d75354aa1c391
witanntech <apreza@witann.com>
2026-01-26 21:51:42 -0600

    Ready for first install

---
+++ access_control_api/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Access Control API",
+    "version": "19.0.1.0.0",
+    "summary": "API para validación de accesos (biometría / hardware)",
+    "category": "Tools",
+    "author": "Witann Technologies",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
+
