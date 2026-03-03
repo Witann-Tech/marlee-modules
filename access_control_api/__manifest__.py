3104e5db018a8965b43a9080a2583d89b4c9d0f4
witanntech <apreza@witann.com>
2026-03-02 19:51:21 -0600

    Resuelve error RPC_ERROR en Odoo

--- access_control_api/__manifest__.py
+++ access_control_api/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann - Access Control API', 
-    'version': '19.0.2.0.3', 
+    'version': '19.0.2.0.4', 
     'summary': 'API para validación de accesos (biometría / hardware)', 
     'category': 'Tools', 
     'author': 'Witann Technologies', 
