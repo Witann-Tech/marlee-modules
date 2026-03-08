712f3fa93651450d09d481ec5083f86687c2c2ca
witanntech <apreza@witann.com>
2026-03-07 19:12:24 -0600

    fix access_control_api menu data for odoo 19 act_window

--- access_control_api/__manifest__.py
+++ access_control_api/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann - Access Control API', 
-    'version': '19.0.2.0.11',
+    'version': '19.0.2.0.12',
     'summary': 'API para validación de accesos (biometría / hardware)', 
     'category': 'Tools', 
     'author': 'Witann Technologies', 
