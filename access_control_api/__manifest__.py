5a03e93b6a8f904fe59d6bef5d6f13f0bca95877
witanntech <apreza@witann.com>
2026-03-12 17:48:56 -0600

    Restaurar vista foto contacto

--- access_control_api/__manifest__.py
+++ access_control_api/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann - Access Control API', 
-    'version': '19.0.2.0.24',
+    'version': '19.0.2.0.25',
     'summary': 'API para validación de accesos (biometría / hardware)', 
     'category': 'Tools', 
     'author': 'Witann Technologies', 
