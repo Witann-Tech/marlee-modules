ca000c2c227f535916439999690c09f1fa4fc912
witanntech <apreza@witann.com>
2026-03-25 19:15:43 -0600

    Bump POS subscriptions module version

--- witann_group_subscriptions_pos/__manifest__.py
+++ witann_group_subscriptions_pos/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann Group Subscriptions POS',
-    'version': '19.0.1.5.18',
+    'version': '19.0.1.5.19',
     'summary': 'Directorio y detalle de suscripciones nativas en Punto de Venta',
     'category': 'Point of Sale',
     'author': 'Witann Technologies',
