9b26b8b853b652a77ff9faffa84fefdeedb43fb6
witanntech <apreza@witann.com>
2026-04-16 12:17:12 -0600

    Revert CURP partner field to restore Odoo startup

--- witann_group_subscriptions/models/__init__.py
+++ witann_group_subscriptions/models/__init__.py
@@ -1,4 +1,3 @@
-from . import res_partner
 from . import product_template
 from . import sale_order
 from . import subscription_plan
--- witann_group_subscriptions/tests/__init__.py
+++ witann_group_subscriptions/tests/__init__.py
@@ -1,3 +1,2 @@
 from . import test_subscription_participants
 from . import test_subscription_access_control
-from . import test_res_partner_curp
