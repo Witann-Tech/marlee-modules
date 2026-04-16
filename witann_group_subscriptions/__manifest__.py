197ed5f721419aae1138c42fb92712bf5b9e277a
witanntech <apreza@witann.com>
2026-04-16 12:48:02 -0600

    Move CURP field below RFC in contact form

--- witann_group_subscriptions/__manifest__.py
+++ witann_group_subscriptions/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann Group Subscriptions',
-    'version': '19.0.1.1.12',
+    'version': '19.0.1.1.13',
     'summary': 'Participantes permitidos en suscripciones de gimnasio',
     'category': 'Sales/Subscriptions',
     'author': 'Witann Technologies',
