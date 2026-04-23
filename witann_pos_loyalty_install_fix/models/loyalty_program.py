27f71474218614e6095ea8d5475b04bdb76dd42f
witanntech <apreza@witann.com>
2026-04-22 21:27:11 -0600

    Replace loyalty monkey patch with registry-safe hook

--- witann_pos_loyalty_install_fix/models/loyalty_program.py
+++ witann_pos_loyalty_install_fix/models/loyalty_program.py
@@ -1,33 +1,33 @@
-try:
-    from odoo.addons.pos_loyalty.models import loyalty_program as pos_loyalty_program
-except ImportError:  # pragma: no cover - pos_loyalty may be absent in some databases
-    pos_loyalty_program = None
+from odoo import models
 
 
-def _wplif_inverse_pos_report_print_id(self):
-    loyalty_mail_model = self.env['loyalty.mail']
-    for program in self:
-        if program.program_type not in ('gift_card', 'ewallet'):
-            continue
-        if not program.pos_report_print_id:
-            continue
+class LoyaltyProgram(models.Model):
+    _inherit = 'loyalty.program'
 
-        values = {
-            'trigger': 'create',
-            'pos_report_print_id': program.pos_report_print_id.id,
-        }
-        if program.mail_template_id:
-            values['mail_template_id'] = program.mail_template_id.id
+    def _inverse_pos_report_print_id(self):
+        loyalty_mail_model = self.env['loyalty.mail']
+        for program in self:
+            if program.program_type not in ('gift_card', 'ewallet'):
+                continue
+            if not program.pos_report_print_id:
+                continue
 
-        if program.communication_plan_ids:
-            program.communication_plan_ids.write(values)
-        else:
-            values['program_id'] = program.id
-            loyalty_mail_model.create(values)
+            values = {
+                'trigger': 'create',
+                'pos_report_print_id': program.pos_report_print_id.id,
+            }
+            if program.mail_template_id:
+                values['mail_template_id'] = program.mail_template_id.id
 
+            if program.communication_plan_ids:
+                program.communication_plan_ids.write(values)
+            else:
+                values['program_id'] = program.id
+                loyalty_mail_model.create(values)
 
-if pos_loyalty_program is not None:
-    pos_loyalty_program.LoyaltyProgram._inverse_pos_report_print_id = _wplif_inverse_pos_report_print_id
-    pos_report_field = pos_loyalty_program.LoyaltyProgram._fields.get('pos_report_print_id')
-    if pos_report_field:
-        pos_report_field.inverse = _wplif_inverse_pos_report_print_id
+    def _register_hook(self):
+        result = super()._register_hook()
+        field = self._fields.get('pos_report_print_id')
+        if field:
+            field.inverse = type(self)._inverse_pos_report_print_id
+        return result
