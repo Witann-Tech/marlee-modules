761e4ba7f5b7097c49c0f50f4afec08fb3185a7a
witanntech <apreza@witann.com>
2026-06-12 16:37:58 -0600

    Move direct debit schema ownership to POS

--- witann_group_subscriptions/__manifest__.py
+++ witann_group_subscriptions/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann Group Subscriptions',
-    'version': '19.0.1.1.36',
+    'version': '19.0.1.1.37',
     'summary': 'Participantes permitidos en suscripciones de gimnasio',
     'category': 'Sales/Subscriptions',
     'author': 'Witann Technologies',
--- witann_group_subscriptions_pos/models/__init__.py
+++ witann_group_subscriptions_pos/models/__init__.py
@@ -1,4 +1,5 @@
 from . import sale_order
+from . import product_template
 from . import pos_subscription_buffer
 from . import res_config_settings
 from . import hr_employee
