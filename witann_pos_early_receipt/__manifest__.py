07851ad83d12515a96bd1024ef5034cba1234445
witanntech <apreza@witann.com>
2026-07-31 12:32:00 -0600

    Revert "[FIX] Preserve native POS config loader fields"
    This reverts commit 4282a4d3f34c53ca45498e917a7a324631230cf0.

--- witann_pos_early_receipt/__manifest__.py
+++ witann_pos_early_receipt/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann POS Early Receipt',
-    'version': '19.0.1.0.1',
+    'version': '19.0.1.0.0',
     'summary': 'Imprime una precuenta antes del pago en POS no restaurante',
     'category': 'Point of Sale',
     'author': 'Witann Technologies',
