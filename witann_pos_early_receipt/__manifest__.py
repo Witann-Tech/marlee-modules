04e945faf9a2b8ce226367d978a576fea40f1c65
witanntech <apreza@witann.com>
2026-07-31 12:32:26 -0600

    Revert "[FIX] Disable unverified early receipt POS asset"
    This reverts commit 47a758f5876c695548de4e5adb2ac5f04edf67d5.

--- witann_pos_early_receipt/__manifest__.py
+++ witann_pos_early_receipt/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Witann POS Early Receipt',
-    'version': '19.0.1.0.2',
+    'version': '19.0.1.0.1',
     'summary': 'Imprime una precuenta antes del pago en POS no restaurante',
     'category': 'Point of Sale',
     'author': 'Witann Technologies',
@@ -9,6 +9,12 @@
     'data': [
         'views/res_config_settings_views.xml',
     ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'witann_pos_early_receipt/static/src/js/early_receipt_button.js',
+            'witann_pos_early_receipt/static/src/xml/early_receipt_button.xml',
+        ],
+    },
     'installable': True,
     'application': False,
 }
